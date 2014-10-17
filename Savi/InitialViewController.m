9cd6f48b5fb827eaef3148b23d5ca4cac63b68bb
Angel Solorio <angel.solorio@me.com>
2014-10-16 21:32:30 -0500

    Sets the project bases

--- Savi/InitialViewController.m
+++ Savi/InitialViewController.m
@@ -6,13 +6,13 @@
 //  Copyright (c) 2014 Grupo Sellcom. All rights reserved.
 //
 
-#import "ViewController.h"
+#import "InitialViewController.h"
 
-@interface ViewController ()
+@interface InitialViewController ()
 
 @end
 
-@implementation ViewController
+@implementation InitialViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
