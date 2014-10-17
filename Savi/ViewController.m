4dc8468c6b4bac99679f3f7cd191f611e3e75604
Angel Solorio <angel.solorio@me.com>
2014-10-16 20:45:29 -0500

    Initial Commit

---
+++ Savi/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Savi
+//
+//  Created by Angel Solorio on 10/16/14.
+//  Copyright (c) 2014 Grupo Sellcom. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ Savi/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Savi
+//
+//  Created by Angel Solorio on 10/16/14.
+//  Copyright (c) 2014 Grupo Sellcom. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
